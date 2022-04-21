ce01ee83654d9f1dab215fa674247145cd8637d5
Noemi <mimi2002@estudiantec.cr>
2022-04-21 00:05:50 -0600

    Documento complementario a la muestra del uso de la memoria

---
+++ Ncurses/main.cpp
@@ -0,0 +1,46 @@
+
+#include <curses.h>
+#include <ncurses.h>
+#include <unistd.h>
+
+using namespace std;
+
+typedef struct MEMPACKED
+{
+char name1[20];
+unsigned long MemTotal;
+}MEM_OCCUPY;
+
+void get_meminfo(MEM_OCCUPY *lpMemory)
+{
+    FILE *fd; // Object file
+    char buff[128]; // Load info from file
+    //fd = fopen("/proc/self/stat", "r"); // Read memory usage File
+    fd = fopen("/proc/meminfo", "r"); // Read memory usage File
+
+    initscr(); // init screen and sets up screen
+    clear();
+
+    for(int i = 0; i < 45; i++) // Read file info and prints in screen
+    {
+        fgets(buff, sizeof(buff), fd);
+        sscanf(buff, "%s %lu ", lpMemory->name1, &lpMemory->MemTotal);
+        printw(buff);
+        refresh(); // refreshes the screen
+    }
+
+    fclose(fd); // Close File
+    getch(); // pause the screen output
+    sleep(2); // Sleep time in seconds
+    endwin(); // deallocates memory and ends ncurses
+}
+
+int main(int argc, char *argv[])
+{
+    MEM_OCCUPY *lpMemory;
+    for(int i = 0; i < 10; i++) // Read file info and prints in screen
+    {
+        get_meminfo(lpMemory);
+    }
+    return 0;
+}
